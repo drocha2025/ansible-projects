b4f1cf0dbd7e484c49b633f029e2b2570aeb6f03
drocha2025 <manutd_2@msn.com>
2025-12-14 15:01:20 -0800

    Update webex_send_message.py

--- webex/webex_send_message.py
+++ webex/webex_send_message.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import requests
 from getpass import getpass
 
@@ -24,31 +23,4 @@ webex_payload = {
 }
 
 rsp = requests.post(url, headers=webex_headers, json=webex_payload)
-=======
-import requests
-from getpass import getpass
-
-pat = getpass("Personal Access Token: ") or "MjJkN2JjNDUtM2M0Mi00Mzc4LTk1NDMtNTI4Zjc5MjFhZjBmNjk1MTdmNTctZmUw_P0A1_ba774546-71bb-4b52-9725-a7e2c4a6fc52"
-
-to = input("Recipient email address: ") or "MyBot2026@webex.bot"
-
-message = input("Message: ") or "AUTOCOR"
-
-base_url = "https://webexapis.com/v1"
-path = "/messages"
-url = f"{base_url}{path}"
-
-webex_headers = {
-    "Accept": "application/json",
-    "Content-Type": "application/json",
-    "Authorization": f"Bearer {pat}"
-}
-
-webex_payload = {
-    "toPersonEmail": to,
-    "text": message
-}
-
-rsp = requests.post(url, headers=webex_headers, json=webex_payload)
->>>>>>> 71579013ceb74eacfdb5497cbdd634346bfe5746
 print(f"Received HTTP status code {rsp.status_code}/ {rsp.reason}")
