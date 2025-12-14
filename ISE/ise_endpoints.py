1d55f0ee27ece059e5b70f817a7a4228e61ed65b
drocha2025 <manutd_2@msn.com>
2025-12-14 14:55:39 -0800

    Update ise_endpoints.py

--- ISE/ise_endpoints.py
+++ ISE/ise_endpoints.py
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-import requests
-import urllib3
-from getpass import getpass
-from requests.auth import HTTPBasicAuth
-
-# Disable insecure HTTPS warnings (sandbox has expired cert)
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-hostname = input("IP address and hostname: ") or "devnetsandboxise.cisco.com"
-username = input("Username: ") or "readonly"
-password = getpass("") or "ISEisC00L"
-
-basic_auth = HTTPBasicAuth(username, password)
-
-base_url = f"https://{hostname}"
-path = "/api/v1/endpoint"
-url = f"{base_url}{path}"
-
-ise_headers = {
-        "Accept": "application/json"
-}
-
-print(f" Sending GET request to {url}")
-
-# Add authentication + ignore SSL verification
-rsp = requests.get(url, auth=basic_auth, headers=ise_headers, verify=False)
-
-endpoints = rsp.json()
-print(f" Received a {type(endpoints)} response containing {len(endpoints)} elements and HTTP status code {rsp.status_code}")
-
-input("Press enter to show all MAC addresses...")
-
-for endpoint in endpoints:
-=======
 import requests
 import urllib3
 from getpass import getpass
